ff81fea936bbb9c6fe3f578e28dcc29eddea081f
qiang.zhang <qiang.zhang@opsmateai.com>
2026-06-05 11:03:14 +0800

    fix: add transition lock in RoomTransition and assert null checks in SceneTransition

--- scenes/ui/transition.gd
+++ scenes/ui/transition.gd
@@ -4,6 +4,10 @@ class_name SceneTransition
 @onready var rect: ColorRect = $ColorRect
 @onready var anim: AnimationPlayer = $ColorRect/AnimationPlayer
 
+func _ready() -> void:
+	assert(rect != null, "SceneTransition: $ColorRect node missing")
+	assert(anim != null, "SceneTransition: $ColorRect/AnimationPlayer node missing")
+
 func fade_in() -> void:
 	anim.play("fade_in")
 	await anim.animation_finished
